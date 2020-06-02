57a123bd74e76cc2ae7963dfacb65fddc472ec1e
liuyang <liuyang__work@163.com>
2020-06-02 16:29:13 +0800

    Java crazy notes

---
+++ javastudy/unit05/subunit01/Person.java
@@ -0,0 +1,13 @@
+package javastudy.unit05.subunit01;
+
+/*
+ *@FileName:Person
+ *@Author: Liu Yang
+ *@Email:liuyang__work@163.com
+ *@Last Modified time: 2020/6/2 16:28
+ */
+
+
+public class Person {
+
+}
